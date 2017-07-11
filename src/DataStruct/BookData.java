fcfa9b440ed939441d66168bd2a77d56b358cb03
Akihiro Takai <at.sisy@gmail.com>
2017-07-11 20:09:50 +0900

    本データ保持クラスを定義

---
+++ src/DataStruct/BookData.java
@@ -0,0 +1,76 @@
+package DataStruct;
+
+import com.google.api.services.books.model.Volume;
+import com.google.api.services.books.model.Volume.SaleInfo;
+import com.google.api.services.books.model.Volume.VolumeInfo;
+
+import java.util.List;
+
+/**
+ * Created by Akihiro on 7/11/2017.
+ */
+public class BookData {
+
+    private List<String> authors;
+    private String title;
+    private String description;
+    private String published_date;
+    private double rating;
+    private double price;
+    private String link_more_info;
+
+    public BookData(Volume volume) {
+        VolumeInfo volume_information = volume.getVolumeInfo();
+        SaleInfo sale_info = volume.getSaleInfo();
+
+        /*
+        * タイトル
+        */
+        title = volume_information.getTitle();
+
+        /*
+        * 著者情報
+        */
+        authors = volume_information.getAuthors();
+
+        /*
+        * 発売日情報
+        */
+        if (volume_information.getPublishedDate() != null && volume_information.getPublishedDate().isEmpty()) {
+            published_date = volume_information.getPublishedDate();
+        } else {
+            published_date = "Unknown";
+        }
+
+        /*
+        * 概要情報
+        */
+        if (volume_information.getDescription() != null && volume_information.getDescription().length() > 0) {
+            description = volume_information.getDescription();
+        } else {
+            description = "";
+        }
+
+        /*
+        * 評価情報
+        */
+        if (volume_information.getRatingsCount() != null && volume_information.getRatingsCount() > 0) {
+            rating = volume_information.getRatingsCount();
+        }
+
+        /*
+        * 価格情報
+        */
+        if (sale_info != null && "FOR_SALE".equals(sale_info.getSaleability())) {
+            if (sale_info.getListPrice().getAmount() - sale_info.getRetailPrice().getAmount() > 0.0) {
+                price = sale_info.getRetailPrice().getAmount();
+            } else {
+                price = -1;
+            }
+        }
+
+        link_more_info = volume_information.getInfoLink();
+
+    }
+
+}
